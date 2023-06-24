d47238c35dc67e738a94c8ebec7facbf2039ace2
santhitak <santhita.sk@gmail.com>
2023-06-25 01:40:45 +0700

    fix: eslint

--- src/core/components/Item.tsx
+++ src/core/components/Item.tsx
@@ -3,35 +3,60 @@ import { HiTrashOutline } from "@qwikest/icons/heroicons";
 import type { Memo } from "../types/memoTypes";
 import { animate, timeline, spring } from "motion";
 
-const Item = component$<{ memo: Memo; deleteMemo: (memo: Memo) => void }>(({ memo, deleteMemo }) => {
-  const element = useSignal<HTMLElement>();
-  useVisibleTask$(() => {
-    const [title, description, date] =
-      [element.value?.querySelector("div")].concat(...(element.value?.querySelectorAll("p") || [])) || [];
+const Item = component$<{ memo: Memo; deleteMemo: (memo: Memo) => void }>(
+  ({ memo, deleteMemo }) => {
+    const element = useSignal<HTMLElement>();
+    useVisibleTask$(() => {
+      const [title, description, date] =
+        [element.value?.querySelector("div")].concat(
+          ...(element.value?.querySelectorAll("p") || [])
+        ) || [];
 
-    if (!title || !description || !date) return;
+      if (!title || !description || !date) return;
 
-    animate(element.value!, { height: ["2em", "10em"] }, { duration: 0.5, easing: spring() });
-    timeline([
-      [element.value!, { y: [30, 0] }, { duration: 0.5, easing: spring({ mass: 0.8, velocity: 4 }) }],
-      [title, { y: [10, 0], opacity: [0, 1] }, { at: 0.5 }],
-      [description, { y: [10, 0], opacity: [0, 1] }, { at: 0.6, easing: spring({ mass: 0.8, velocity: 8 }) }],
-      [date, { y: [10, 0], opacity: [0, 1] }, { at: 0.7, easing: spring({ mass: 0.8, velocity: 10 }) }],
-    ]);
-  });
-  return (
-    <div ref={element} class="w-full shadow border rounded p-4 space-y-3">
-      <div class="flex items-center justify-between">
-        <h3 class="font-medium text-xl text-primary">{memo.title}</h3>
-        <HiTrashOutline
-          onClick$={() => deleteMemo(memo)}
-          class="text-xl text-red-500 cursor-pointer p-2 rounded-full hover:bg-red-50 transition box-content"
-        />
+      animate(
+        element.value!,
+        { height: ["2em", "10em"] },
+        { duration: 0.5, easing: spring() }
+      );
+      timeline([
+        [
+          element.value!,
+          { y: [30, 0] },
+          { duration: 0.5, easing: spring({ mass: 0.8, velocity: 4 }) },
+        ],
+        [title, { y: [10, 0], opacity: [0, 1] }, { at: 0.5 }],
+        [
+          description,
+          { y: [10, 0], opacity: [0, 1] },
+          { at: 0.6, easing: spring({ mass: 0.8, velocity: 8 }) },
+        ],
+        [
+          date,
+          { y: [10, 0], opacity: [0, 1] },
+          { at: 0.7, easing: spring({ mass: 0.8, velocity: 10 }) },
+        ],
+      ]);
+    });
+
+    return (
+      <div ref={element} class="w-full shadow border rounded p-4 space-y-3">
+        <div class="flex items-center justify-between">
+          <h3 class="font-medium text-xl text-primary">{memo.title}</h3>
+          <HiTrashOutline
+            onClick$={() => deleteMemo(memo)}
+            class="text-xl text-red-500 cursor-pointer p-2 rounded-full hover:bg-red-50 transition box-content"
+          />
+        </div>
+        <p class="line-clamp-15 break-words text-gray-600">
+          {memo.description}
+        </p>
+        <p class="text-gray-400 text-sm">
+          วันที่ : {new Date(memo.date).toLocaleDateString()}
+        </p>
       </div>
-      <p class="line-clamp-15 break-words text-gray-600">{memo.description}</p>
-      <p class="text-gray-400 text-sm">วันที่ : {new Date(memo.date).toLocaleDateString()}</p>
-    </div>
-  );
-});
+    );
+  }
+);
 
 export default Item;
--- src/core/components/shared/hero.tsx
+++ src/core/components/shared/hero.tsx
@@ -1,5 +1,5 @@
 import { component$, useVisibleTask$, useSignal } from "@builder.io/qwik";
-import { stagger, timeline, spring, animate } from "motion";
+import { stagger, timeline, spring } from "motion";
 
 export default component$<{ isMouseOver: boolean }>(({ isMouseOver }) => {
   const outputRef = useSignal<Element>();
@@ -7,7 +7,11 @@ export default component$<{ isMouseOver: boolean }>(({ isMouseOver }) => {
   useVisibleTask$(({ track }) => {
     const hoverAnimation = timeline(
       [
-        ["#title", { y: [10, 0, -2, 6, -4], x: [10, 0, 4, -2, 4] }, { duration: 0.3 }],
+        [
+          "#title",
+          { y: [10, 0, -2, 6, -4], x: [10, 0, 4, -2, 4] },
+          { duration: 0.3 },
+        ],
         [
           "#title > span",
           { color: ["black", "#824d61", "#fe5a99", "#824d61", "black"] },
@@ -33,25 +37,46 @@ export default component$<{ isMouseOver: boolean }>(({ isMouseOver }) => {
 
   useVisibleTask$(() => {
     timeline([
-      ["#title ", { y: [40, 0] }, { duration: 0.3, easing: spring({ mass: 0.8 }) }],
+      [
+        "#title ",
+        { y: [40, 0] },
+        { duration: 0.3, easing: spring({ mass: 0.8 }) },
+      ],
       [
         "#title > span",
         { opacity: [0, 1] },
-        { duration: 1, easing: spring({ mass: 1 }), delay: stagger(0.1), at: "<" },
+        {
+          duration: 1,
+          easing: spring({ mass: 1 }),
+          delay: stagger(0.1),
+          at: "<",
+        },
       ],
     ]);
 
     timeline([
       ["#shit", { x: [24, 0] }, { duration: 0.5, easing: "ease-out" }],
-      ["#shit", { y: [-24, -18, 0] }, { duration: 0.5, easing: ["linear"], at: "<" }],
-      ["#shit", { rotate: ["-85deg", "-12deg"], scale: [0.6, 1] }, { duration: 0.5, at: "<" }],
+      [
+        "#shit",
+        { y: [-24, -18, 0] },
+        { duration: 0.5, easing: ["linear"], at: "<" },
+      ],
+      [
+        "#shit",
+        { rotate: ["-85deg", "-12deg"], scale: [0.6, 1] },
+        { duration: 0.5, at: "<" },
+      ],
     ]);
   });
 
   return (
     <div id="hero" class="flex justify-center items-center py-12 w-full">
       <div class="relative flex items-end justify-end">
-        <div ref={outputRef} id="title" class="text-[20vw] md:text-9xl font-bold">
+        <div
+          ref={outputRef}
+          id="title"
+          class="text-[20vw] md:text-9xl font-bold"
+        >
           <span>M</span>
           <span>e</span>
           <span>m</span>
@@ -59,7 +84,10 @@ export default component$<{ isMouseOver: boolean }>(({ isMouseOver }) => {
         </div>
         <p
           id="shit"
-          style={{ textShadow: " -1px -1px 0 white, 1px -1px 0 black, -1px 1px 0 white, 1px 1px 0 white;" }}
+          style={{
+            textShadow:
+              " -1px -1px 0 white, 1px -1px 0 black, -1px 1px 0 white, 1px 1px 0 white;",
+          }}
           class="text-[4vw] md:text-xl font-semibold bg-primary/30 text-primary backdrop-blur py-0.5 px-3 absolute bottom-0 -right-2 -rotate-12"
         >
           SHT7!
